2a748f6faaf71b510fe293da1703af3863657707
Tabrigos <guerrerapaolo@gmail.com>
2023-01-19 15:07:13 +0100

    aggiunta getmapping

--- src/main/java/net/tab/exercise/StrudentsController.java
+++ src/main/java/net/tab/exercise/StrudentsController.java
@@ -3,15 +3,17 @@ package net.tab.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StrudentsController {
 
+	@GetMapping("/students")
 	public List<Student> getAllStudents() {
 
 		List<Student> students = new ArrayList<>();
-		
+		students.add(new Student(2, "Peppe", "Pizza", "Napoli", "Idle"));
 		
 		return students;
 	}
