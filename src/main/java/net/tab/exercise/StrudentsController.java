2460b4d5321113daec376618d96fc3905ee6d53e
Tabrigos <guerrerapaolo@gmail.com>
2023-01-23 18:04:17 +0100

    @PostMapping for addStudent

--- src/main/java/net/tab/exercise/StrudentsController.java
+++ src/main/java/net/tab/exercise/StrudentsController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,10 +20,12 @@ public class StrudentsController {
 		return studentService.getAllStudents();
 	}
 	
-	public Student getStudent(int id) {
+	@GetMapping("/students/{id}")
+	public Student getStudent(@PathVariable int id) {
 		return studentService.getStudent(id);
 	}
 	
+	@PostMapping("/students")
 	public void addStudent(Student student) {
 		studentService.addStudent(student);
 	}
